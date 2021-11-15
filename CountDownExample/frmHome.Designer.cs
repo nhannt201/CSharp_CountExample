70f22e727957ea46ee2f82565520f5595f4ae996
Nguyễn Trung Nhẫn <trungnhan21.12@gmail.com>
2021-11-15 09:06:09 +0700

    Add files via upload

---
+++ CountDownExample/frmHome.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace CountDownExample
+{
+    partial class frmHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbMin = new System.Windows.Forms.Label();
+            this.lbSec = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tmCount = new System.Windows.Forms.Timer(this.components);
+            this.btnReset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbMin
+            // 
+            this.lbMin.AutoSize = true;
+            this.lbMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMin.Location = new System.Drawing.Point(148, 62);
+            this.lbMin.Name = "lbMin";
+            this.lbMin.Size = new System.Drawing.Size(231, 163);
+            this.lbMin.TabIndex = 0;
+            this.lbMin.Text = "00";
+            // 
+            // lbSec
+            // 
+            this.lbSec.AutoSize = true;
+            this.lbSec.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSec.Location = new System.Drawing.Point(427, 62);
+            this.lbSec.Name = "lbSec";
+            this.lbSec.Size = new System.Drawing.Size(231, 163);
+            this.lbSec.TabIndex = 1;
+            this.lbSec.Text = "00";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(321, 309);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(159, 56);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(529, 309);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(159, 56);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // tmCount
+            // 
+            this.tmCount.Interval = 1000;
+            this.tmCount.Tick += new System.EventHandler(this.tmCount_Tick);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(98, 309);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(159, 56);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(342, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 163);
+            this.label1.TabIndex = 5;
+            this.label1.Text = ":";
+            // 
+            // frmHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.lbSec);
+            this.Controls.Add(this.lbMin);
+            this.Name = "frmHome";
+            this.Text = "CountUpDown";
+            this.Load += new System.EventHandler(this.frmHome_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbMin;
+        private System.Windows.Forms.Label lbSec;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer tmCount;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CountDownExample/frmHome.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CountDownExample
+{
+    public partial class frmHome : Form
+    {
+        public frmHome()
+        {
+            InitializeComponent();
+        }
+
+        int count = 0;
+
+        int min = 0;
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            tmCount.Start();
+        }
+
+        private void tmCount_Tick(object sender, EventArgs e)
+        {
+            count++;
+            if (count >= 60)
+            {
+                min++;
+                if (min.ToString().Length == 1)
+                {
+                    lbMin.Text = "0" + min.ToString();
+                } else
+                {
+                    lbMin.Text =  min.ToString();
+                }
+                
+                count = 0;
+            }
+           
+            if (count.ToString().Length == 1)
+            {
+                lbSec.Text = "0" + count.ToString();
+            }
+            else
+            {
+                lbSec.Text = count.ToString();
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            tmCount.Stop();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            count = 0;
+            min = 0;
+        }
+
+        private void frmHome_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
